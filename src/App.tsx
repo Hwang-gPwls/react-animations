9a4d82fcff4b4a0cacda1e3a7674a38dc26adfb9
Hyejin-Hwang <h960502hhj@gmail.com>
2022-10-19 10:12:56 +0900

    feat: animation 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
-import GlobalStyle from "./styles/global-style";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: #dbd2c9;
+`;
+
+const Box = styled(motion.div)`
+	width: 200px;
+	height: 200px;
+	background-color: white;
+	border-radius: 15px;
+	box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			hello
-		</ThemeProvider>
+		<Wrapper>
+			<Box
+				transition={{ type: "spring", delay: 1 }}
+				initial={{ scale: 0 }}
+				animate={{ scale: 1, rotateZ: 360 }}
+			/>
+		</Wrapper>
 	);
 }
 
